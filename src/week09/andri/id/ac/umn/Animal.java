471662588ced04872903aa3e34149070085532e6
azizandri <azizandri72@gmail.com>
2025-11-05 11:18:41 +0700

    Multiple Inheritance

--- src/week09/andri/id/ac/umn/Animal.java
+++ src/week09/andri/id/ac/umn/Animal.java
@@ -1,5 +1,30 @@
 package week09.andri.id.ac.umn;
 
 public interface Animal {
+	public void eat();
+	public void travel();
+}
 
+public class Mammal implements Animal{
+	
+
+	public Mammal() {}
+	
+	@Override
+	public void eat() {
+		System.out.println("Mammal eats");
+	}
+
+	@Override
+	public void travel() {
+		System.out.println("Mammal travel");
+		
+	}
+	
+	public static void main(String[] args) {
+		Mammal m = new Mammal();
+		m.eat();
+		m.travel();
+	};
+	
 }
